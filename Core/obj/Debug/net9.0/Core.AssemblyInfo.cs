d5cb35f3914b8e026f034c9cb8965377837ad3f2
TipsyFuddledBoozyGroggy <153048923+TipsyFuddledBoozyGroggy@users.noreply.github.com>
2025-03-05 10:55:34 -0700

    added material tailwind extensions

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcc0021eae7ab3865916b4187a87543e345e4f3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f30535db3e68da5250796bd5a2fd8fc2ff18866")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
